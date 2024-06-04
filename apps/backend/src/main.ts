55281ce17eb1753d72ed365c23374cf0b06cd61a
Isachenko Ivan <vanisachenko@gmail.com>
2024-06-04 13:04:05 +0600

    feat: NGINX initial setup

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -10,12 +10,19 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 8000;
+  app.enableCors({
+    credentials: true,
+    origin:[
+      "http://localhost:4200",
+      "http://localhost:4200/",
+      "http://127.0.0.1:4200",
+      "http://127.0.0.1:4200/"
+    ]
+  })
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://localhost:${port}`
   );
 }
 
