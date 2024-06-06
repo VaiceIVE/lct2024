01ade88b44b7d15760a32422e8e644893ecf206f
romaiy <sokolovgit256@gmail.com>
2024-06-06 21:18:58 +0600

    feat: added date picker

--- apps/frontend/src/pages/forecast/index.tsx
+++ apps/frontend/src/pages/forecast/index.tsx
@@ -1,11 +1,36 @@
+import { useCallback, useEffect, useState } from 'react';
+import { debounce } from 'lodash';
+
 import { PageWrapper } from 'shared/ui/Wrappers/PageWrapper';
 import { ForecastCards } from 'widgets/forecast-cards';
+import { ForecastDatePicker } from 'widgets/forecast-date-picker';
 import { ForecastList } from 'widgets/forecast-list';
 import { ForecastMap } from 'widgets/forecast-map';
+import { months } from 'shared/constants/months';
 
 const ForecastPage = () => {
+  const [monthsIndex, setMonthsIndex] = useState(0);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debounceMonthsIndex = useCallback(
+    debounce((newState) => getAnalysisResult(newState), 600),
+    []
+  );
+
+  function getAnalysisResult(index: number) {
+    console.log(months[index].value);
+  }
+
+  useEffect(() => {
+    debounceMonthsIndex(monthsIndex);
+  }, [debounceMonthsIndex, monthsIndex]);
+
   return (
     <PageWrapper>
+      <ForecastDatePicker
+        monthsIndex={monthsIndex}
+        setMonthsIndex={setMonthsIndex}
+      />
       <ForecastCards />
       <ForecastMap />
       <ForecastList />
--- apps/frontend/src/widgets/forecast-cards/index.tsx
+++ apps/frontend/src/widgets/forecast-cards/index.tsx
@@ -6,20 +6,17 @@ export const ForecastCards = () => {
   const data = ['Событий', 'Задействованные районы', 'Потребители'];
 
   return (
-    <Stack gap={44}>
-      <div>23</div>
-      <Grid gutter={18}>
-        {data.map((item) => (
-          <Grid.Col span={4}>
-            <Card>
-              <Stack gap={8}>
-                <Title level={1} title="12" />
-                <div className="text medium secondary">{item}</div>
-              </Stack>
-            </Card>
-          </Grid.Col>
-        ))}
-      </Grid>
-    </Stack>
+    <Grid gutter={18}>
+      {data.map((item) => (
+        <Grid.Col key={item} span={4}>
+          <Card>
+            <Stack gap={8}>
+              <Title level={1} title="12" />
+              <div className="text medium secondary">{item}</div>
+            </Stack>
+          </Card>
+        </Grid.Col>
+      ))}
+    </Grid>
   );
 };
