a5b0c7f4ab5b9613b302d564255b309c4678a73d
romaiy <sokolovgit256@gmail.com>
2024-06-09 01:01:37 +0600

    refactor: split into components

--- apps/frontend/src/widgets/prediction-create-cards/index.tsx
+++ apps/frontend/src/widgets/prediction-create-cards/index.tsx
@@ -1,16 +1,7 @@
-import { Flex, Grid, Stack, useMantineTheme } from '@mantine/core';
-import {
-  IconCircleCheckFilled,
-  IconFileFilled,
-  IconFileUpload,
-  IconTrashX,
-} from '@tabler/icons-react';
-import { Card } from 'shared/ui/Card';
-import { Title } from 'shared/ui/Title';
+import { Grid } from '@mantine/core';
 import { TemperatureConditions } from './components/temperature-conditions';
 import { ITemperatureConditions } from 'shared/models/ITemperatureConditions';
-import { Upload } from 'shared/ui/Upload';
-import { useRef } from 'react';
+import { PredictTerritory } from './components/prediction-territory';
 
 interface PredictionCreateCardProps {
   conditions: ITemperatureConditions[];
@@ -27,62 +18,14 @@ export const PredictionCreateCards = ({
   setFiles,
   handleDeleteFile,
 }: PredictionCreateCardProps) => {
-  const theme = useMantineTheme();
-
-  const resetRef = useRef<() => void>(null);
-
   return (
     <Grid gutter={16}>
       <Grid.Col span={9}>
-        <Card type="outline">
-          <Stack gap={28}>
-            <Flex align={'center'} gap={12}>
-              <Title level={3} title="Территория прогноза" />
-              {files.length ? (
-                <IconCircleCheckFilled
-                  color={theme.colors.myBlue[1]}
-                  size={24}
-                />
-              ) : null}
-            </Flex>
-            <Stack w={679} gap={18}>
-              <p className="text medium secondary">
-                Вы можете загрузить новый файл с информацией по домам. Наш
-                алгоритм проанализирует их и выдаст обновленный прогноз{' '}
-              </p>
-              <Upload
-                type="light"
-                label="Загрузить файл .xlsx, .xls"
-                icon={<IconFileUpload size={18} />}
-                onChange={setFiles}
-                resetRef={resetRef}
-              />
-              {files.length ? (
-                <Stack gap={12}>
-                  <p className="text medium">Загружен файл:</p>
-                  {files.map((f, index) => (
-                    <Flex key={index} gap={8} align={'center'}>
-                      <IconFileFilled
-                        color={theme.colors.myBlack[4]}
-                        size={18}
-                      />
-                      <p className="text medium secondary">{f.name}</p>
-                      <IconTrashX
-                        cursor={'pointer'}
-                        onClick={() => {
-                          handleDeleteFile(index);
-                          resetRef.current?.();
-                        }}
-                        size={18}
-                        color={theme.colors.myState[1]}
-                      />
-                    </Flex>
-                  ))}
-                </Stack>
-              ) : null}
-            </Stack>
-          </Stack>
-        </Card>
+        <PredictTerritory
+          files={files}
+          setFiles={setFiles}
+          handleDeleteFile={handleDeleteFile}
+        />
       </Grid.Col>
       <Grid.Col span={9}>
         <TemperatureConditions
