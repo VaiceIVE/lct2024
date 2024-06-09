71cbf4e914557267c2815f711b97ac2c19114d87
romaiy <sokolovgit256@gmail.com>
2024-06-09 17:15:29 +0600

    fix: prod fixes

--- apps/frontend/src/shared/store/user/index.ts
+++ apps/frontend/src/shared/store/user/index.ts
@@ -76,7 +76,7 @@ export default class UserStore {
     } catch (e: any) {
       console.info(e);
 
-      //this.setAuth(false);
+      this.setAuth(false);
       this.setUser({} as IUser);
     } finally {
       this.setLoading(false);
