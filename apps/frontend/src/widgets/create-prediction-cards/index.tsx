f7808716670843d90ddb3c1f44ffb52eb23a5115
romaiy <sokolovgit256@gmail.com>
2024-06-13 23:46:25 +0600

    feat: alert card added

--- apps/frontend/src/widgets/create-prediction-cards/index.tsx
+++ apps/frontend/src/widgets/create-prediction-cards/index.tsx
@@ -1,7 +1,9 @@
-import { Grid } from '@mantine/core';
+import { Flex, Grid, useMantineTheme } from '@mantine/core';
 import { TemperatureConditions } from './components/temperature-conditions';
 import { ITemperatureConditions } from 'shared/models/ITemperatureConditions';
 import { PredictTerritory } from './components/prediction-territory';
+import { Card } from 'shared/ui/Card';
+import { IconCircleCheckFilled } from '@tabler/icons-react';
 
 interface CreatePredictionCardsProps {
   conditions: ITemperatureConditions[];
@@ -18,6 +20,8 @@ export const CreatePredictionCards = ({
   setFiles,
   handleDeleteFile,
 }: CreatePredictionCardsProps) => {
+  const theme = useMantineTheme();
+
   return (
     <Grid gutter={16}>
       <Grid.Col span={9}>
@@ -26,6 +30,16 @@ export const CreatePredictionCards = ({
           setFiles={setFiles}
           handleDeleteFile={handleDeleteFile}
         />
+      </Grid.Col>
+      <Grid.Col span={3}>
+        <Card type="error" p="16px 18px">
+          <Flex gap={16} align={'flex-start'}>
+            <IconCircleCheckFilled color={theme.colors.myState[1]} size={24} />
+            <p className="text error">
+              Для нового прогноза введите данные как минимум в один блок
+            </p>
+          </Flex>
+        </Card>
       </Grid.Col>
       <Grid.Col span={9}>
         <TemperatureConditions
