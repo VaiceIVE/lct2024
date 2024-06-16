012111d1190fed7732f0cd0be903fe856170fe7d
Isachenko Ivan <vanisachenko@gmail.com>
2024-06-16 19:23:54 +0600

    fix: await ...

--- apps/backend/src/prediction/prediction.service.ts
+++ apps/backend/src/prediction/prediction.service.ts
@@ -51,7 +51,7 @@ export class PredictionService {
             {
                 if(!names.includes(name))
                     {
-                        await this.storageService.uploadToS3Buffer(createReadStream(join(process.cwd(),`/apps/backend/src/defaultTables/${name}`)).read(), name)
+                        await this.storageService.uploadToS3Buffer(await createReadStream(join(process.cwd(),`/apps/backend/src/defaultTables/${name}`)).read(), name)
                     }
             }
         let predictionStatus = axios.post(this.configService.get('PREDICTION_BACKEND_URL'), {list_of_tables: defaultNames, period: 2024})
