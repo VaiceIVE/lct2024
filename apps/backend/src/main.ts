0ff1b8afd21d62c85ca12a890efcb2dd86f99bf5
Isachenko Ivan <vanisachenko@gmail.com>
2024-06-23 01:16:48 +0600

    fix: req size

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,11 +3,14 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 8000;
   app.use(cookieParser())
+  app.use(bodyParser.json({limit: '100mb'}));
+  app.use(bodyParser.urlencoded({limit: '100mb', extended: true}));
   app.enableCors({
     credentials: true,
     origin:[
